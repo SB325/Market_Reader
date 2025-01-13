8967f38af48eaf6e9129e93411cca3966a2467c2
Sheldon Bish <sbish33@gmail.com>
2025-01-12 23:54:28 -0500

    Switching from Vespa.ai to Elasticsearch

--- pipelines/fundamentals/vectorize/vectorize.py
+++ pipelines/fundamentals/vectorize/vectorize.py
@@ -1,10 +1,13 @@
-# from vespa.application import ApplicationPackage
-from app_package import app_package
-from vespa.package import Schema, Document, Field, FieldSet, HNSW, RankProfile, Component, Parameter
-from vespa.deployment import VespaDocker
+from langchain_ollama import OllamaEmbeddings
+
+# add elastic
+from langchain_elasticsearch import ElasticsearchStore
+from langchain_core.documents import Document
+from uuid import uuid4
+
 import os
 from dotenv import load_dotenv
-from vespa.io import VespaResponse
+
 import pdb
 import asyncio
 from load_model import tokenizer, model
@@ -17,122 +20,14 @@ import os.path
 
 load_dotenv(override=True)
 
-VESPA_CONTAINER_IP = os.getenv("VESPA_CONTAINER_IP")
-VESPA_API_PORT = os.getenv("VESPA_API_PORT")
-RUNNING_VESPA_API = True
+ELASTIC_IP = os.getenv("ELASTIC_IP")
+ELASTIC_PORT = os.getenv("ELASTIC_PORT")
+ELASTIC_PW = os.getenv("ELASTIC_PASSWORD")
 
-def write_session_id(id: str):
-    with open('session_id.txt', 'w') as f:
-        f.write(id)
+embeddings = OllamaEmbeddings(model="llama3.1:8b")
 
-def get_session_id():
-    id = ''
-    file = 'session_id.txt'
-    if os.path.isfile(file):
-        with open(file, 'r') as f:
-            id = f.read()
-    return id
-
-def make_session_object(id: str, endpoint: str):
-    obj = {
-        'session-id' :  id,
-        'content' : f"/application/v2/tenant/default/session/[session-id]/",
-        'prepare' : f"/application/v2/tenant/default/session/[session-id]/",
-    }   
-    obj['content'].replace('[session-id]/',f"{id}/content/")
-    obj['prepare'].replace('[session-id]/',f"{id}/prepared/")
-    return obj
-
-class DeployVespa():
-    session_id : str
-    prepared_url : str
-    content: str
-    session_prepare_url : str
-    ip: str
-    config_port: str
-    upload_endpoint: str = '/application/v2/tenant/default/session'
-
-    def __init__(self, ip: str = VESPA_CONTAINER_IP, port: str = VESPA_API_PORT) -> str:
-        self.ip = ip
-        self.config_port = port
-        url_in = f"http://{ip}:{port}/{self.upload_endpoint}n"
-        header = {'Content-Type': 'application/zip'}
-        files ={"archive": app_package.to_zip()}
-        
-        id = get_session_id()
-        if id:
-            session_obj = make_session_object(id, 
-                    "/application/v2/tenant/default/session/[session-id]/"
-                    )
-            self.session_id = session_obj['session-id']
-            self.content = f"http://{self.ip}:{self.config_port}{session_obj['content']}"
-            self.session_prepare_url = f"http://{self.ip}:{self.config_port}{session_obj['prepare']}"
-        else:
-            response = requests.post(
-                        url=url_in, 
-                        files=files, 
-                        headers=header,
-                        timeout=5,
-                    )
-            if not response.ok:
-                msg = f"Error {response.status_code}\n{url_in}:\n{response.reason}"
-                return msg
-            else: 
-                msg = response.json()
-                session_obj = make_session_object(msg['session-id'], 
-                    "/application/v2/tenant/default/session/[session-id]/"
-                    )
-                self.session_id = session_obj['session-id']
-                self.content = f"http://{self.ip}:{self.config_port}{session_obj['content']}"
-                self.session_prepare_url = f"http://{self.ip}:{self.config_port}{session_obj['prepare']}"
-
-                write_session_id(self.session_id)
-
-    def prepare_session(self, applicationName: str, 
-                        environment: str, 
-                        region: str, 
-                        instance: str, 
-                        debug: bool = True, 
-                        timeout: int = 360,
-                    ):
-        parameters = {'applicationName': applicationName, 
-                        'environment': environment, 
-                        'region': region, 
-                        'instance': instance, 
-                        'debug': debug, 
-                        'timeout': timeout,
-                    }
-        pdb.set_trace()
-        response = requests.put(
-                url=self.session_prepare_url, 
-                json=parameters,
-            )
-        if not response.ok:
-            msg = f"Error {response.status_code}:\n{response.reason}"
-            pdb.set_trace()
-        else: 
-            msg = response.json()
-            pdb.set_trace()
-
-    def close_session(self, id: str):
-        url = f"http://{self.ip}:{self.config_port}/application/v2/tenant/default/session/{id}/content/"
-        response = requests.delete(
-            url = url
-        )
-        if not response.ok:
-            msg = f"Error {response.status_code}:\n{response.reason}"
-            pdb.set_trace()
-        else: 
-            msg = response.json()
-            pdb.set_trace()
-
-def response_callback(response: VespaResponse, id: str):
-    if not response.is_successful():
-        print(f"Response for id {id} not successful")
-    else:
-        print(f"Response for id {id} successful")
-
-text_splitter = RecursiveCharacterTextSplitter(
+def get_splitter():
+    return RecursiveCharacterTextSplitter(
                 chunk_size=768,  # chars, not llm tokens
                 chunk_overlap=0,
                 length_function=len,
@@ -140,15 +35,81 @@ text_splitter = RecursiveCharacterTextSplitter(
                 )
 
 def chunk(str_to_chunk: str):
-    # chunks = text_splitter.transform_documents([str_to_chunk])
-    chunks = text_splitter.split_documents([str_to_chunk])
+    chunks = get_splitter().split_documents([str_to_chunk])
     return chunks
 
-def add_data_to_vector_db(data: list, use_api: bool = RUNNING_VESPA_API):
+class ElasticClass():
+    ip: str
+    port: str
+
+    def __init__(self, ip: str = ELASTIC_IP, port: str = ELASTIC_PORT, verbose: bool = False):
+        self.ip = ip
+        self.port = port
+        self.es = ElasticsearchStore(
+            es_url=f"http://{ip}:9200",
+            index_name="langchain_index",
+            embedding=embeddings,
+            es_user="elastic",
+            es_password=ELASTIC_PW,
+            # if you use the model_id
+            # strategy=DenseVectorStrategy(model_id="test_model")
+            # if you use hybrid search
+            # strategy=DenseVectorStrategy(hybrid=True)
+        )
+
+        if verbose:
+            self.es.info().body
+
+    def add_documents(self, document_obj: list, index: str):
+        doclist = []
+        for doc in document_obj:
+            doclist.append(
+                    Document(
+                        page_content=doc['content'],
+                        metadata={"source": doc['source']},
+                    )
+            )
+        
+        uuids = [str(uuid4()) for _ in range(len(doclist))]
+
+        self.es.add_documents(documents=doclist, ids=uuids)
+
+    def delete_from_vectorstore(self, uuid_list: list):
+        self.es.delete(ids=uuid_list)
+
+    def query(self, field: str, query_str: str):
+        results = self.es.similarity_search(
+            query=query_str,
+            k=2,
+            filter=[{"term": {"metadata.source.keyword": field}}],
+        )
+        for res in results:
+            print(f"* {res.page_content} [{res.metadata}]")
+    
+    def query_score(self, field: str, query_str: str):
+        results = self.es.similarity_search_with_score(
+            query="Will it be hot tomorrow",
+            k=1,
+            filter=[{"term": {"metadata.source.keyword": "news"}}],
+        )
+        for doc, score in results:
+            print(f"* [SIM={score:3f}] {doc.page_content} [{doc.metadata}]")
+
+    def retriever(self, prompt: str, threshold: float = 0.2):
+        retriever = vector_store.as_retriever(
+            search_type="similarity_score_threshold", search_kwargs={"score_threshold": threshold}
+        )
+        retriever.invoke(prompt)
+
+    def __del__(self, id: str):
+        self.es.transport.close()
+
+
+
+def add_data_to_vector_db(data: list):
     status = False
     resp = ''
     for dat in data:
-    #for dat in tqdm(data, desc='embedding...'):
         str_to_embed = dat['fields']['filing_content_string']
         if str_to_embed:
             doc_to_embed = langchain_doc(
@@ -174,19 +135,13 @@ def add_data_to_vector_db(data: list, use_api: bool = RUNNING_VESPA_API):
         # in app object
         pass
     else:
-        app.feed_iterable(data, 
-                schema="doc0", 
-                callback=response_callback
-                ) 
+        # app.feed_iterable(data, 
+        #         schema="doc0", 
+        #         callback=response_callback
+        #         ) 
     return status
     
 
 if __name__ == "__main__":
-    if RUNNING_VESPA_API:
-        app = DeployVespa(ip = VESPA_CONTAINER_IP, port = VESPA_API_PORT)
-        app.prepare_session(applicationName = 'vector0',
-                            environment = 'default', 
-                            region = 'default', 
-                            instance = 'default',
-                            )
+
     pdb.set_trace()
